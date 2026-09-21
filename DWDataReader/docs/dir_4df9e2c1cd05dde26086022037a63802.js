d6deeecd998ac4a7be9559ef518d78a2a261229d
sche0151 <sche0151@student.monash.edu>
2026-09-21 15:11:52 +1000

    [Update] changed ignore rules

---
+++ DWDataReader/docs/dir_4df9e2c1cd05dde26086022037a63802.js
@@ -0,0 +1,4 @@
+var dir_4df9e2c1cd05dde26086022037a63802 =
+[
+    [ "shared", "dir_a94ae95919912e0039d3262afff7c565.html", "dir_a94ae95919912e0039d3262afff7c565" ]
+];
---
+++ DWDataReader/docs/dir_a94ae95919912e0039d3262afff7c565.js
@@ -0,0 +1,7 @@
+var dir_a94ae95919912e0039d3262afff7c565 =
+[
+    [ "DWDataReaderLib.h", "_d_w_data_reader_lib_8h_source.html", null ],
+    [ "DWDataReaderLibDef.h", "_d_w_data_reader_lib_def_8h_source.html", null ],
+    [ "DWDataReaderLibFuncs.h", "_d_w_data_reader_lib_funcs_8h.html", "_d_w_data_reader_lib_funcs_8h" ],
+    [ "DWDataReaderLibTypes.h", "_d_w_data_reader_lib_types_8h.html", "_d_w_data_reader_lib_types_8h" ]
+];
